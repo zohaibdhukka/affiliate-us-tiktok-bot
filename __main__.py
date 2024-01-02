8f0c04dce6c1afd05569ee380fbf7568fa46ea6e
daridev <darideveloper@gmail.com>
2024-01-01 21:06:49 -0600

    feat (bot): login and filters

--- __main__.py
+++ __main__.py
@@ -1,13 +1,24 @@
 import os
 from dotenv import load_dotenv
 from logs import logger
-from scraping_manager.automate import WebScraping
+from libs.bot import Bot
 
 load_dotenv ()
 VAR = os.getenv ("VAR")
 
-def main (): 
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    logger.info("Starting bot...")
+    chrome_folder = 'C:\\Users\\herna\\AppData\\Local\\Google\\Chrome\\User Data'
+    bot = Bot(
+        chrome_folder
+    )
+    
+    # Login validation
+    is_logged = bot.login()
+    if not is_logged:
+        logger.error("Error: Login failed")
+        quit()
+        
+    bot.filter_creators()
+    
+    print("done")
---
+++ libs/bot.py
@@ -0,0 +1,100 @@
+from libs.web_scraping import WebScraping
+
+
+class Bot(WebScraping):
+    
+    def __init__(self, chrome_folder: str):
+        """ Start chrome and load home page
+        
+        Args:
+            chrome_folder (str): path to chrome data folder
+        """
+        
+        # Start chrome
+        super().__init__(
+            chrome_folder=chrome_folder,
+            start_killing=True
+        )
+        
+        # Load home page
+        self.home_page = "https://affiliate-us.tiktok.com/" \
+            "connection/creator?shop_region=US"
+        self.set_page(self.home_page)
+        
+        self.refresh_selenium(time_units=2)
+    
+    def __select_dropdown__(self, selector_dropdown: str, elem_text: str):
+        """ Select specific drop down element
+        
+        Args:
+            selector_dropdown (str): selector for dropdown
+            elem_text (str): text of element to select
+        """
+        
+        selectors = {
+            "options": 'li > label + *',
+            "input": 'li:nth-child(index) > label > input'
+        }
+        
+        # Display dropdown options
+        self.click(selector_dropdown)
+        self.refresh_selenium()
+        
+        # Select specific option
+        option_found = False
+        options = self.get_elems(selectors["options"])
+        for option in options:
+            if option.text == elem_text:
+                option_index = options.index(option)
+                selector_input = selectors["input"].replace(
+                    "index",
+                    str(option_index + 1)
+                )
+                self.click_js(selector_input)
+                option_found = True
+                break
+        return option_found
+    
+    def login(self):
+        """ Validate correct login and close popups
+        """
+        
+        selectors = {
+            "profile_image": '.m4b-avatar-image img',
+        }
+        image_elem = self.get_elem(selectors["profile_image"])
+        if not image_elem:
+            return False
+        
+        return True
+    
+    def filter_creators(self):
+        """ Apply filters to search creators
+        """
+        
+        selectors = {
+            "creators_btn": '.m4b-dropdown + .arco-spin.w-full button',
+            "categories": '#categories > div > button',
+            "followers": '#followerSize > div > button',
+            "content_type": '#contentType > div > button',
+            "creator_agency": '#creatorAgency > div > button',
+        }
+        
+        self.click(selectors["creators_btn"])
+        self.refresh_selenium()
+        self.__select_dropdown__(selectors["categories"], "Beauty & Personal Care")
+        self.__select_dropdown__(selectors["followers"], "10K-100K")
+        self.__select_dropdown__(selectors["content_type"], "All")
+        self.__select_dropdown__(selectors["creator_agency"], "All")
+    
+    def save_creators(self):
+        pass
+    
+    def show_saved_creators(self):
+        pass
+    
+    def invtate_creators(self):
+        pass
+    
+    def send_invitation(self):
+        pass
